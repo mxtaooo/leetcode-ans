9a9032b6957bdc11eb8eba6633bbeca70cd05506
mxtao <taomengxu@126.com>
2020-09-07 14:53:24 +0800

    implementations using for-loop

--- src/fsharp/1 - Two Sum.fs
+++ src/fsharp/1 - Two Sum.fs
@@ -1,12 +1,37 @@
 module Solution
 
-let twoSum (array: int[]) target = 
+open System.Collections.Generic
+
+let V1 (array: int[]) target = 
+    let mutable result = null
+    for i = 0 to (array.Length - 1) do
+        for j = 0 to (array.Length - 1) do
+            if array.[i] + array.[j] = target then result <- [| i; j|]
+    result
+
+let V2 (array: int[]) target =
+    let mutable result = null
+    let mutable found = false
+    let dict = new Dictionary<int, int>()
+    for i = 0 to (array.Length - 1) do
+        if not found then
+            if dict.ContainsKey(target - array.[i]) then
+                found <- true
+                result <- [| i; dict.[target - array.[i]] |]
+            else
+                dict.TryAdd(array.[i], i) |> ignore
+    result
+
+// 更具F#自身特色的实现
+let V3 (array: int[]) target = 
     let dict = array |> Seq.mapi (fun index num -> (num, index)) |> Map.ofSeq
     array
     |> Seq.mapi (fun index num -> (num, index))
     |> Seq.tryFind (fun (num, index) -> dict.ContainsKey(target - num) && dict.[target - num] <> index)
     |> Option.map (fun (num, index) -> [|index; dict.[target - num]|])
     |> Option.get
+
+let twoSum array target = v1 array target
 
 let print array target = 
     let result = twoSum array target
