a731b0b388b916e6b94031e4f3a1e40bca140187
mxtao <taomengxu@126.com>
2020-09-13 19:59:37 +0800

    commit csharp solution for question-7

---
+++ src/csharp/7 - Reverse Integer.cs
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int Reverse(int x)
+    {
+        var result = 0;
+        for (; x != 0; x /= 10)
+        {
+            var pop = x % 10;
+            if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && pop > 7))
+            {
+                return 0;
+            }
+            if (result < int.MinValue / 10 || (result == int.MinValue / 10 && pop < -8))
+            {
+                return 0;
+            }
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+}
