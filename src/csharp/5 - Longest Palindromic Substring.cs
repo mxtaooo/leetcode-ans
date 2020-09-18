a3bda516ab83a01703aee29438c8c8a219835382
mxtao <taomengxu@126.com>
2020-09-18 21:06:29 +0800

    add dynamic solution for question-5

--- src/csharp/5 - Longest Palindromic Substring.cs
+++ src/csharp/5 - Longest Palindromic Substring.cs
@@ -1,5 +1,6 @@
 class Solution
 {
+    // 中心扩展法
     public string LongestPalindrome(string s)
     {
         var span = ReadOnlySpan<char>.Empty;
@@ -23,5 +24,52 @@ class Solution
             }
         }
         return new string(span);
+    }
+
+    // 动态规划法
+    static string LongestPalindrome(string s)
+    {
+        var dp = new bool[s.Length, s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            dp[i, i] = true;
+            for (int l = 0; l <= Math.Min(i, s.Length-1-i) - 1; l++)
+            {
+                dp[i-l-1, i+l+1] = dp[i-l, i+l] && s[i-l-1] == s[i+l+1];
+            }
+            if (i+1 < s.Length && s[i] == s[i+1])
+            {
+                dp[i, i+1] = true;
+                for (int l = 0; l < Math.Min(i, s.Length-1-(i+1)); l++)
+                {
+                    dp[i-l-1, i+1+l+1] = dp[i-l, i+1+l] && s[i-l-1] == s[i+1+l+1];
+                }
+            }
+        }
+
+        var (start, len) = (0, 0);
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = i; j < s.Length; j++)
+            {
+                if (dp[i, j] && j - i + 1 > len)
+                {
+                    start = i;
+                    len = j - i + 1;
+                }
+            }
+        }
+
+        return s.Substring(start, len);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"abcba: {LongestPalindrome("abcba")}");
+        Console.WriteLine($"babad: {LongestPalindrome("babad")}");
+        Console.WriteLine($"cbbd: {LongestPalindrome("cbbd")}");
+        Console.WriteLine($": {LongestPalindrome("")}");
+        Console.WriteLine($"a: {LongestPalindrome("a")}");
     }
 }
