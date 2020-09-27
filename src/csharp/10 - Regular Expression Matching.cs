94288eb5685257f5b1fd5cdf7e64885119559b42
mxtao <taomengxu@126.com>
2020-09-27 21:50:10 +0800

    commit recursion solution

--- src/csharp/10 - Regular Expression Matching.cs
+++ src/csharp/10 - Regular Expression Matching.cs
@@ -1,16 +1,28 @@
 class Solution
 {
+    static bool IsMatchOnSpan(ReadOnlySpan<char> s, ReadOnlySpan<char> p)
+    {
+        if (p.IsEmpty) return s.IsEmpty;
+
+        // 验证两者是否有相同的首字符
+        var first = (!s.IsEmpty && (p[0] == s[0] || p[0] == '.'));
+        if (p.Length >= 2 && p[1] == '*')
+        {
+            // 对于
+            return IsMatchOnSpan(s, p.Slice(2)) || (first && IsMatchOnSpan(s.Slice(1), p));
+        }
+        else
+        {
+            return first && IsMatchOnSpan(s.Slice(1), p.Slice(1));
+        }
+    }
     public static bool IsMatch(string s, string p)
     {
-        static bool IsMatchSimple(ReadOnlySpan<char> s, ReadOnlySpan<char> p)
-        {
-            if (s.IsEmpty && p.IsEmpty) return true;
-            return s[0] == p[0] && IsMatchSimple(s.Slice(1), p.Slice(1));
-        }
-        return IsMatchSimple(s.AsSpan(), p.AsSpan());
+
+        return IsMatchOnSpan(s.AsSpan(), p.AsSpan());
     }
     static void Main(string[] args)
     {
-        Console.WriteLine(IsMatch("abc", "abc"));
+        Console.WriteLine(IsMatch("abc", "a*bc"));
     }
 }
