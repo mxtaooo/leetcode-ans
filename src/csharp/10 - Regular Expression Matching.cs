4cf12d75373d3c77fd2a38c53445b084131b8cc4
mxtao <taomengxu@126.com>
2020-10-02 17:55:26 +0800

    dp solution for question-10

--- src/csharp/10 - Regular Expression Matching.cs
+++ src/csharp/10 - Regular Expression Matching.cs
@@ -26,3 +26,51 @@ class Solution
         Console.WriteLine(IsMatch("abc", "a*bc"));
     }
 }
+
+using System;
+
+namespace CSharpSolution
+{
+    class Solution
+    {
+        static bool IsMatch(string s, string p)
+        {
+            bool IsMatch(int si, int pi) => si >= 0 && (p[pi] == '.' || s[si] == p[pi]);
+
+            var dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0, 0] = true;
+
+            for (int sl = 0; sl <= s.Length; sl++)
+            {
+                for (int pl = 1; pl <= p.Length; pl++)
+                {
+                    if (p[pl-1] != '*')
+                    {
+                        dp[sl, pl] = IsMatch(sl-1, pl-1) && dp[sl-1, pl-1] ;
+                    }
+                    else
+                    {
+                        dp[sl, pl] = dp[sl, pl-2] || (IsMatch(sl-1, pl-2) && dp[sl-1, pl]);
+                    }
+                }
+            }
+            return dp[s.Length, p.Length];
+        }
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(IsMatch("abc", "abc"));
+            // Console.WriteLine(IsMatch("abc", "ab."));
+            // Console.WriteLine(IsMatch("abcc", "abc*"));
+            // Console.WriteLine(IsMatch("ab", "abc*"));
+            // Console.WriteLine(IsMatch("ab", "abc*d"));
+            Console.WriteLine(IsMatch("mississippi", "mis*is*p*."));
+            Console.WriteLine(IsMatch("aab", "c*a*b"));
+            Console.WriteLine(IsMatch("aa", "a"));
+            Console.WriteLine(IsMatch("aaa", "ab*a*c*a"));
+            Console.WriteLine(IsMatch("a", ".*..a*"));
+
+        }
+    }
+}
+
