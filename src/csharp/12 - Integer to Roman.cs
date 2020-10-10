b84cce520e4ebf3beb00c7c340b4c8432396b299
mxtao <taomengxu@126.com>
2020-10-10 20:49:27 +0800

    new solution

--- src/csharp/12 - Integer to Roman.cs
+++ src/csharp/12 - Integer to Roman.cs
@@ -64,6 +64,18 @@ class Solution
         }
         return sb.ToString();
     }
+
+    static string IntToRoman2(int num)
+    {
+        var array = new string[,] {
+            {"", "M", "MM", "MMM", "", "", "", "", "", ""},
+            {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+        };
+        return array[0, num/1000] + array[1, num%1000/100] + array[2, num%100/10] + array[3, num%10];
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine($"1 = {IntToRoman(1)}");
