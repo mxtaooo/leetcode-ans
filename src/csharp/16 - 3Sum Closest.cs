7c6415eb0f863b98e0083d8d3ca6d190c96d1326
mxtao <taomengxu@126.com>
2020-10-27 11:37:54 +0800

    commit optimized solution

--- src/csharp/16 - 3Sum Closest.cs
+++ src/csharp/16 - 3Sum Closest.cs
@@ -1,6 +1,7 @@
 class Solution
 {
 
+    // 暴力解法
     static int ThreeSumClosest(int[] nums, int target)
     {
         // Array.Sort(nums);
@@ -20,6 +21,50 @@ class Solution
             }
         }
 
+        return result;
+    }
+
+    // O(N^2) 双指针解法
+    static int ThreeSumClosest(int[] nums, int target)
+    {
+        // 确保游标移动到下一个数字
+        void PreNumber(ref int index)
+        {
+            do
+            {
+                index--;
+            } while(index >= 0 && nums[index] == nums[index+1]);
+        }
+        // 确保游标移动到上一个数字
+        void NextNumber(ref int index)
+        {
+            do
+            {
+                index++;
+            } while(index < nums.Length && nums[index] == nums[index-1]);
+        }
+
+        Array.Sort(nums);
+        var result = nums[0] + nums[1] + nums[2];
+        for (var i = 0; i < nums.Length-2; NextNumber(ref i))
+        {
+            for (var (j, k) = (i+1, nums.Length-1); j < k; )
+            {
+                var sum = nums[i] + nums[j] + nums[k];
+                if (Math.Abs(target-sum) < Math.Abs(target-result))
+                {
+                    result = sum;
+                }
+                if (sum > target)
+                {
+                    PreNumber(ref k);
+                }
+                else
+                {
+                    NextNumber(ref j);
+                }
+            }
+        }
         return result;
     }
 
