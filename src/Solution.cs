47b0d679f1e8ecd8f12229840dfe698d9ce650ba
mxtao <taomengxu@126.com>
2020-10-30 16:29:21 +0800

    update solutions

--- src/Solution.cs
+++ src/Solution.cs
@@ -45,31 +45,6 @@ namespace CSharpSolution
             return result;
         }
 
-        static List<string> GenerateParenthesis(int n)
-        {
-            static (string, string) SplitString(string str, int index)
-            {
-                return (str.Substring(0, index), str.Substring(index));
-            }
-
-            var result = new HashSet<string>() {"()"};
-            for (int i = 2; i <= n; i++)
-            {
-                var set = new HashSet<string>();
-                foreach (var partial in result)
-                {
-                    for (int j = 0; j < partial.Length; j++)
-                    {
-                        var (prefix, surfix) = SplitString(partial, j);
-                        set.Add(prefix + "()" + surfix);
-                    }
-                }
-                result = set;
-            }
-
-            return new List<string>(result);
-        }
-
         static void Main(string[] args)
         {
             var list = ListNode.Build(1, 2, 3, 4, 5);
