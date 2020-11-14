929765d927db7cb16a62204a7a37997995f813d7
mxtao <taomengxu@126.com>
2020-11-14 13:35:40 +0800

    commit solution

--- src/csharp/28 - Implement strStr().cs
+++ src/csharp/28 - Implement strStr().cs
@@ -1,26 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace CSharpSolution
+class Solution
 {
-    class Solution
+    static int StrStr(string haystack, string needle)
     {
-        static int StrStr(string haystack, string needle)
-        {
-            if (String.IsNullOrEmpty(needle)) return 0;
+        if (String.IsNullOrEmpty(needle)) return 0;
 
-            for (int i = 0; i < haystack.Length - needle.Length; i++)
-            {
-                if (haystack.Substring(i, needle.Length) == needle) return i;
-            }
-            return -1;
-        }
-
-        static void Main(string[] args)
+        for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
         {
-            Console.WriteLine(StrStr("hello", "lo"));
+            if (haystack.Substring(i, needle.Length) == needle) return i;
         }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(StrStr("hello", "o"));
     }
 }
-
