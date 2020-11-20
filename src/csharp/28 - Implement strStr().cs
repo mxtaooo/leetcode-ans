bf118a0ad95480d4b9f03fa3d245e8cea6793c48
mxtao <taomengxu@126.com>
2020-11-20 15:16:21 +0800

    update solution

--- src/csharp/28 - Implement strStr().cs
+++ src/csharp/28 - Implement strStr().cs
@@ -1,5 +1,6 @@
 class Solution
 {
+    // 子串逐一比较法
     static int StrStr(string haystack, string needle)
     {
         if (String.IsNullOrEmpty(needle)) return 0;
@@ -7,6 +8,27 @@ class Solution
         for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
         {
             if (haystack.Substring(i, needle.Length) == needle) return i;
+        }
+        return -1;
+    }
+
+    // 字串逐一比较法 - 小优化、不再频繁创建字符串对象；对于失败对比可立即停止
+    static int StrStr(string haystack, string needle)
+    {
+        if (String.IsNullOrEmpty(needle)) return 0;
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            var matched = true;
+            for (int j = 0; j < needle.Length; j++)
+            {
+                if (haystack[i+j] != needle[j]) 
+                {
+                    matched = false;
+                    break;
+                }
+            }
+            if (matched) return i;
         }
         return -1;
     }
