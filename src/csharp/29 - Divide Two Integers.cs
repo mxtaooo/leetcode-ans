f8de3e84d4a54b3b2fc1f483d0d13c0151d47ea0
mxtao <taomengxu@126.com>
2020-11-30 17:30:45 +0800

    stage new solution

--- src/csharp/29 - Divide Two Integers.cs
+++ src/csharp/29 - Divide Two Integers.cs
@@ -52,6 +52,68 @@ class Solution
         return result;
     }
 
+    // ============================================
+
+    // 判断两数字是否同号
+    static bool IsSameFlag(int x, int y) => (x > 0 && y > 0) || (x < 0 && y < 0);
+
+    // 被除数一路自加、尽快“放大”自身到除数的“另一侧”
+    // TODO: 需要确认数字“放大”过程中的溢出情况
+    static int HitBoundary(Dictionary<int, int> dict, int dividend, int divisor, int cur)
+    {
+        if ((dividend >= 0 && dict[cur] >= dividend) || (dividend < 0 && dict[cur] <= dividend))
+        {
+            return cur;
+        }
+        else
+        {
+            var next = cur + cur;
+            var value = dict[cur] + dict[cur];
+            dict[next] = value;
+            return HitBoundary(dict, dividend, divisor, next);
+        }
+    }
+
+    static int BinarySearch(Dictionary<int, int> dict, int dividend, int divisor, (int, int) range)
+    {
+        var (bottom, top) = range;
+        if (top - bottom == 1)
+        {
+            return IsSameFlag(dividend, divisor) ? bottom : top;
+        }
+        else
+        {
+            var half = ???;
+        }
+    }
+
+    static int Divide(int dividend, int divisor)
+    {
+        if (dividend == 0) return 0;
+        switch (divisor)
+        {
+            case 1:
+                return dividend;
+            case -1 when dividend == int.MinValue:
+                return int.MaxValue;
+            case -1:
+                return -dividend;
+            default:
+                break;
+        }
+
+        var dict = new Dictionary<int, int>
+        {
+            {-1, -divisor},
+            {0, 0},
+            {1, divisor}
+        };
+
+        var boundary = HitBoundary(dict, dividend, divisor, IsSameFlag(dividend, divisor) ? 1 : -1);
+
+        return boundary;
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine(Divide(10, 3));
