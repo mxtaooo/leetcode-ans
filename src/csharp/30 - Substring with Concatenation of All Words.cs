305e2265ccabd60092d076bf89319ab7a683cc2d
mxtao <taomengxu@126.com>
2020-12-17 21:51:05 +0800

    update partial solution

--- src/csharp/30 - Substring with Concatenation of All Words.cs
+++ src/csharp/30 - Substring with Concatenation of All Words.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-
-namespace CSharpConsole
+namespace CSharpConsoleApp
 {
 
-    class Solution
+    static class Helper
     {
-        static List<int> AllIndexOf(string s, string value)
+        public static List<int> AllIndexOf(this string s, string value)
         {
             var result = new List<int>();
             var slen = s.Length;
@@ -30,7 +28,7 @@ namespace CSharpConsole
             return InnerFunc(result, 0);
         }
 
-        static Dictionary<int, List<int>> Copy(Dictionary<int, List<int>> dict)
+        public static Dictionary<int, List<int>> Copy(this Dictionary<int, List<int>> dict)
         {
             var result = new Dictionary<int, List<int>>(dict.Count);
             foreach (var (k, v) in dict)
@@ -40,14 +38,18 @@ namespace CSharpConsole
             return result;
         }
 
+    }
+
+    class Program
+    {
+
         static List<int> FindSubstring(string s, string[] words)
         {
-
             // (id, len, indexes)
             var list = new List<(int, int, List<int>)>(words.Length);
             for (int i = 0; i < words.Length; i++)
             {
-                var indexes = AllIndexOf(s, words[i]);
+                var indexes = s.AllIndexOf(words[i]);
                 if (indexes.Count == 0)
                 {
                     return new List<int>();
@@ -55,44 +57,12 @@ namespace CSharpConsole
                 list.Add((i, words[i].Length, indexes));
             }
 
-            // index in s -> word
-            // var dict = new Dictionary<int, List<int>>();
-            // var len = 0;
-            // for (int i = 0; i < words.Length; i++)
-            // {
-            //     len += words[i].Length;
-            //     var indexes = AllIndexOf(s, words[i]);
-            //     if (indexes.Count == 0)
-            //     {
-            //         return new List<int>();
-            //     }
-            //     foreach (var index in indexes)
-            //     {
-            //         if (dict.TryGetValue(index, out var value))
-            //         {
-            //             value.Add(i);
-            //         }
-            //         else
-            //         {
-            //             dict[index] = new List<int> { i };
-            //         }
-            //     }
-            // }
-
-            // var result = new List<int>();
-            // for (int i = 0; i < s.Length - len + 1; i++)
-            // {
-            //     if (dict.ContainsKey(i)) 
-            //     {
-
-            //     }
-            // }
 
         }
 
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
