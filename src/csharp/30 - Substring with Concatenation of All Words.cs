e2446cdfeb17e86cd730fffd9421ed9dc01736cb
mxtao <taomengxu@126.com>
2020-12-18 21:17:50 +0800

    commit solution

--- src/csharp/30 - Substring with Concatenation of All Words.cs
+++ src/csharp/30 - Substring with Concatenation of All Words.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace CSharpConsoleApp
 {
@@ -28,41 +30,112 @@ namespace CSharpConsoleApp
             return InnerFunc(result, 0);
         }
 
-        public static Dictionary<int, List<int>> Copy(this Dictionary<int, List<int>> dict)
+        public static string Format(this List<int> list)
         {
-            var result = new Dictionary<int, List<int>>(dict.Count);
-            foreach (var (k, v) in dict)
+            if (list.Count == 0)
             {
-                result[k] = new List<int>(v);
+                return "[ ]";
             }
-            return result;
+            return "[ " + list.Select(i => i.ToString()).Aggregate((s1, s2) => s1 + ", " + s2) + " ]";
         }
 
     }
 
     class Program
     {
+        static bool IsExist(int index, ImmutableHashSet<int> targets, Dictionary<int, int> lens, Dictionary<int, ImmutableHashSet<int>> dict)
+        {
+            if (targets.IsEmpty)
+            {
+                return true;
+            }
+
+            if (dict.ContainsKey(index))
+            {
+                var intersect = dict[index].Intersect(targets);
+                if (intersect.Count == 1)
+                {
+                    foreach (var i in intersect)
+                    {
+                        return IsExist(index + lens[i], targets.Remove(i), lens, dict);
+                    }
+                }
+                else if (intersect.Count > 1)
+                {
+                    foreach (var i in intersect)
+                    {
+                        if (IsExist(index + lens[i], targets.Remove(i), lens, dict))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
 
         static List<int> FindSubstring(string s, string[] words)
         {
-            // (id, len, indexes)
-            var list = new List<(int, int, List<int>)>(words.Length);
+            // (id, index list)
+            var list = new List<(int, List<int>)>(words.Length);
+            // (id, len)
+            var lens = new Dictionary<int, int>(words.Length);
+            // len
+            var len = 0;
             for (int i = 0; i < words.Length; i++)
             {
-                var indexes = s.AllIndexOf(words[i]);
-                if (indexes.Count == 0)
+                len += words[i].Length;
+                lens.Add(i, words[i].Length);
+                var index = s.AllIndexOf(words[i]);
+                if (index.Count == 0)
                 {
                     return new List<int>();
                 }
-                list.Add((i, words[i].Length, indexes));
+                list.Add((i, index));
             }
 
+            // (index, id list)
+            var dict = new Dictionary<int, List<int>>();
+            foreach (var (id, ilist) in list)
+            {
+                foreach (var index in ilist)
+                {
+                    if (dict.ContainsKey(index))
+                    {
+                        dict[index].Add(id);
+                    }
+                    else
+                    {
+                        dict.Add(index, new List<int>() { id });
+                    }
+                }
+            }
 
+            var dict2 = new Dictionary<int, ImmutableHashSet<int>>(dict.Count);
+            foreach (var (index, ids) in dict)
+            {
+                dict2[index] = ImmutableHashSet.CreateRange(ids);
+            }
+
+            var result = new List<int>();
+            var targets = ImmutableHashSet.CreateRange(Enumerable.Range(0, words.Length));
+            for (int i = 0; i < s.Length - len + 1; i++)
+            {
+                if (dict2.ContainsKey(i) && IsExist(i, targets, lens, dict2))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(FindSubstring("barfoothefoobarman", new string[] { "foo", "bar" }).Format());
+            Console.WriteLine(FindSubstring("wordgoodgoodgoodbestword", new string[] { "word", "good", "best", "word" }).Format());
+            Console.WriteLine(FindSubstring("barfoofoobarthefoobarman", new string[] { "bar", "foo", "the" }).Format());
         }
     }
 }
