d4e1ee4a32948b0385b9dca6b2baf5713aa7d86a
mxtao <taomengxu@126.com>
2020-12-30 21:56:59 +0800

    commit F# version solution - partial

---
+++ src/fsharp/30 - Substring with Concatenation of All Words.fs
@@ -0,0 +1,24 @@
+open System
+
+type String with
+    member this.AllIndexOf0 (value: string) = 
+        let rec func start list =
+            match this.IndexOf(value, (start: int)) with
+            | -1 -> list
+            | i -> func (i+1) (i::list)
+        func 0 []
+    member this.AllIndexOf1 (value: string) =
+        seq {
+            let mutable i = this.IndexOf(value)
+            while i <> -1 do
+                yield i
+                i <- this.IndexOf(value, i+1)
+        } |> List.ofSeq
+    member this.AllIndexOf value = this.AllIndexOf1 value
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    "barfoothefoobarman".AllIndexOf0 "foo" |> printfn "%A"
+    "barfoothefoobarman".AllIndexOf1 "foo" |> printfn "%A"
+    0 // return an integer exit code
