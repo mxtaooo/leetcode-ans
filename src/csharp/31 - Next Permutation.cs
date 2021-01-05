9b19d1d2e9ce7165bd535480126daf95ec780197
mxtao <taomengxu@126.com>
2021-01-05 17:18:32 +0800

    optimize solution

--- src/csharp/31 - Next Permutation.cs
+++ src/csharp/31 - Next Permutation.cs
@@ -10,28 +10,34 @@ namespace CSharpConsoleApp
             {
                 return;
             }
-            var start = 0;
-            for (int index = nums.Length - 1; index > 0; index--)
+
+            // 尝试找到较小数字在前的位置
+            var index = nums.Length - 1;
+            while(index > 0 && nums[index-1] >= nums[index])
             {
-                if (nums[index-1] < nums[index])
+                index--;
+            }
+
+            // 说明全体数字都是从大到小排列、这是最大数字
+            if (index == 0)
+            {
+                Array.Sort(nums);
+                return;
+            }
+
+            // 找到比这个“较小数字”大“一点点”的数字、交换两者位置、并将后半段重排
+            var mi = index;
+            for (int i = index; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[index-1] && nums[i] < nums[mi])
                 {
-                    var (mi, min) = (index, nums[index]);
-                    for (int i = index; i < nums.Length; i++)
-                    {
-                        if (nums[i] > nums[index-1] && nums[i] < min)
-                        {
-                            mi = i;
-                            min = nums[i];
-                        }
-                    }
-                    nums[mi] = nums[index - 1];
-                    nums[index - 1] = min;
-                    start = index;
-                    break;
+                    mi = i;
                 }
             }
-            Array.Sort(nums, start, nums.Length - start);
-            return;
+            var tmp = nums[mi];
+            nums[mi] = nums[index-1];
+            nums[index-1] = tmp;
+            Array.Sort(nums, index, nums.Length - index);
         }
 
         static void Execute(int[] nums)
