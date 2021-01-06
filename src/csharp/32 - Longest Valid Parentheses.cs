3c673152f1c261a604d5c4d122ed36c8f5d70646
mxtao <taomengxu@126.com>
2021-01-06 14:08:43 +0800

    commit partial solution

---
+++ src/csharp/32 - Longest Valid Parentheses.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace CSharpConsoleApp
+{
+    class Program
+    {
+        static int LongestValidParentheses(string s)
+        {
+            var max = 0;
+            var current = 0;
+            var left = 0;
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        left++;
+                        break;
+                    case ')':
+                        if (left > 0)
+                        {
+                            left--;
+                            current += 2;
+                        }
+                        else
+                        {
+                            max = max > current ? max : current;
+                            current = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return max > current ? max : current;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{LongestValidParentheses("(()")}");
+            Console.WriteLine($"{LongestValidParentheses(")()())")}");
+            Console.WriteLine($"{LongestValidParentheses("")}");
+            Console.WriteLine($"{LongestValidParentheses("()(()")}"); // error !
+        }
+    }
+}
