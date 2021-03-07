e14f9697d9a963bcc57e6d5a7d66389e5233c55a
mxtao <taomengxu@126.com>
2021-03-07 18:22:01 +0800

    simple update

--- src/csharp/39 - Combination Sum.cs
+++ src/csharp/39 - Combination Sum.cs
@@ -1,54 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
-#region Original Solution
 namespace CSharpConsoleApp
 {
-    class IntListComparer : IEqualityComparer<IList<int>>
-    {
-        public bool Equals(IList<int>? x, IList<int>? y)
-        {
-            if (ReferenceEquals(x, y)) return true;
-            if (x != null && y != null && x.Count == y.Count)
-            {
-                var list1 = new List<int>(x);
-                list1.Sort();
-                var list2 = new List<int>(y);
-                list2.Sort();
-                for (int i = 0; i < list1.Count; i++)
-                {
-                    if (list1[i] != list2[i])
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return false;
-        }
-
-        public int GetHashCode([DisallowNull] IList<int> list)
-        {
-            var tmp = new List<int>(list);
-            tmp.Sort();
-            unchecked
-            {
-                int hash = 19;
-                foreach (var i in tmp)
-                {
-                    hash = hash * 31 + i.GetHashCode();
-                }
-                return hash;
-            }
-        }
-    }
-
     class Program
     {
+        private class IntListComparer : IEqualityComparer<IList<int>>
+        {
+            public bool Equals(IList<int>? x, IList<int>? y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (x != null && y != null && x.Count == y.Count)
+                {
+                    var list1 = new List<int>(x);
+                    list1.Sort();
+                    var list2 = new List<int>(y);
+                    list2.Sort();
+                    for (int i = 0; i < list1.Count; i++)
+                    {
+                        if (list1[i] != list2[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            public int GetHashCode(IList<int> list)
+            {
+                var tmp = new List<int>(list);
+                tmp.Sort();
+                unchecked
+                {
+                    int hash = 19;
+                    foreach (var i in tmp)
+                    {
+                        hash = hash * 31 + i.GetHashCode();
+                    }
+                    return hash;
+                }
+            }
+        }
+
         private static readonly IEqualityComparer<IList<int>> comparer = new IntListComparer();
 
+        // Original Solution
         static IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
             var raw = new HashSet<int>(candidates);
@@ -84,53 +83,33 @@ namespace CSharpConsoleApp
             return result;
         }
 
-        static string Format(IList<IList<int>> list)
-        {
-            return $"[{string.Join(",", list.Select(l => $"[{string.Join(",", l)}]"))}]";
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(Format(CombinationSum(new int[] { 2, 3, 5 }, 8)));
-            Console.WriteLine(Format(CombinationSum(new int[] { 2 }, 1)));
-            Console.WriteLine(Format(CombinationSum(new int[] { 1 }, 1)));
-            Console.WriteLine(Format(CombinationSum(new int[] { 1 }, 2)));
-        }
-    }
-}
-#endregion
-
-#region DFS Solution
-namespace DFS
-{
-    class Program
-    {
-        static void DFS(int[] candidates, int target, IList<IList<int>> result, IList<int> combine, int idx)
-        {
-            if (idx == candidates.Length)
-            {
-                return;
-            }
-            if (target == 0)
-            {
-                result.Add(new List<int>(combine));
-                return;
-            }
-            DFS(candidates, target, result, combine, idx + 1);
-            if (target - candidates[idx] >= 0)
-            {
-                combine.Add(candidates[idx]);
-                DFS(candidates, target - candidates[idx], result, combine, idx);
-                combine.RemoveAt(combine.Count - 1);
-            }
-        }
-
-        static IList<IList<int>> CombinationSum(int[] candidates, int target)
+        // DFS Solution
+        static IList<IList<int>> CombinationSumII(int[] candidates, int target)
         {
             var result = new List<IList<int>>();
+
+            void DFS(int target, IList<int> combine, int idx)
+            {
+                if (idx == candidates.Length)
+                {
+                    return;
+                }
+                if (target == 0)
+                {
+                    result.Add(new List<int>(combine));
+                    return;
+                }
+                DFS(target, combine, idx + 1);
+                if (target - candidates[idx] >= 0)
+                {
+                    combine.Add(candidates[idx]);
+                    DFS(target - candidates[idx], combine, idx);
+                    combine.RemoveAt(combine.Count - 1);
+                }
+            }
+
             var combine = new List<int>();
-            DFS(candidates, target, result, combine, 0);
+            DFS(target, combine, 0);
             return result;
         }
 
@@ -149,4 +128,3 @@ namespace DFS
         }
     }
 }
-#endregion
