94050eeed0cad8eff9ef0dfae3e1bcf7c63b4e0b
mxtao <taomengxu@126.com>
2021-03-17 21:50:49 +0800

    remove unused code

--- src/fsharp/40 - Combination Sum II.fs
+++ src/fsharp/40 - Combination Sum II.fs
@@ -1,20 +1,20 @@
 open System
 open System.Collections.Generic
 
-let combinationSum ints target = 
-    let raw = Set.ofArray ints
-    let dict = new Dictionary<int, HashSet<int list>>()
-    for i = 1 to target do
-        let set = new HashSet<int list>()
-        for j = 1 to i/2 do
-            if dict.ContainsKey(j) && dict.ContainsKey(i-j) then
-                (dict.[j], dict.[i-j])
-                ||> Seq.allPairs
-                |> Seq.map (fun (l1, l2) -> l1 @ l2 |> List.sort)
-                |> Seq.iter (fun list -> set.Add(list) |> ignore)
-        if raw.Contains(i) then set.Add([i]) |> ignore
-        if set.Count > 0 then dict.[i] <- set
-    dict.GetValueOrDefault(target, new HashSet<int list>()) |> List.ofSeq
+// let combinationSum ints target = 
+//     let raw = Set.ofArray ints
+//     let dict = new Dictionary<int, HashSet<int list>>()
+//     for i = 1 to target do
+//         let set = new HashSet<int list>()
+//         for j = 1 to i/2 do
+//             if dict.ContainsKey(j) && dict.ContainsKey(i-j) then
+//                 (dict.[j], dict.[i-j])
+//                 ||> Seq.allPairs
+//                 |> Seq.map (fun (l1, l2) -> l1 @ l2 |> List.sort)
+//                 |> Seq.iter (fun list -> set.Add(list) |> ignore)
+//         if raw.Contains(i) then set.Add([i]) |> ignore
+//         if set.Count > 0 then dict.[i] <- set
+//     dict.GetValueOrDefault(target, new HashSet<int list>()) |> List.ofSeq
 
 let combinationSumII (ints: int array) target = 
     let result = new List<IList<int>>()
