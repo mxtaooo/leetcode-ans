42ef176e24288301740b3491e72ac5a7c7707bf9
mxtao <taomengxu@126.com>
2021-03-26 12:45:39 +0800

    not correct

--- src/csharp/40 - Combination Sum II.cs
+++ src/csharp/40 - Combination Sum II.cs
@@ -6,10 +6,17 @@ namespace CSharpConsoleApp
 {
     class Program
     {
-
         static IList<IList<int>> CombinationSum2(int[] candidates, int target)
         {
-            var result = new HashSet<IList<int>>(comparer);
+            var dict = new Dictionary<int, int>();
+            foreach (var candidate in candidates)
+            {
+                dict[candidate] = dict.TryGetValue(candidate, out var c) ? c + 1 : 1;
+            }
+            // (candidate, count)
+            var list = dict.Select(kv => (kv.Key, kv.Value)).OrderBy(t => t.Key).ToList();
+
+            var result = new List<IList<int>>();
 
             void DFS(int target, IList<int> combine, int idx)
             {
@@ -18,16 +25,25 @@ namespace CSharpConsoleApp
                     result.Add(new List<int>(combine));
                     return;
                 }
-                if (idx == candidates.Length)
+                if (idx == list.Count)
                 {
                     return;
                 }
                 DFS(target, combine, idx + 1);
-                if (target - candidates[idx] >= 0)
+                for (int i = 0; i < list[idx].Value; i++)
                 {
-                    combine.Add(candidates[idx]);
-                    DFS(target - candidates[idx], combine, idx + 1);
-                    combine.RemoveAt(combine.Count - 1);
+                    if (target - (list[idx].Key * (i+1)) >= 0)
+                    {
+                        for (int c = 0; c <= i; c++)
+                        {
+                            combine.Add(candidates[idx]);
+                        }
+                        DFS(target - candidates[idx], combine, idx + 1);
+                        for (int c = 0; c <= i; c++)
+                        {
+                            combine.RemoveAt(combine.Count - 1);
+                        }
+                    }
                 }
             }
 
